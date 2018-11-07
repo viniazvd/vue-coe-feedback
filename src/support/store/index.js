91d7ee1f8ad4de890857076453e1b0f0c9380e93
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2018-11-07 11:42:15 -0200

    performance optimization

--- src/support/store/index.js
+++ src/support/store/index.js
@@ -1,44 +1,44 @@
-export default {
-  state: {
-    feedbacks: [],
-    delay: 0
+const state = {
+  feedbacks: [],
+  delay: 0
+}
+
+const getters = {
+  __feedbacks: ({ feedbacks }) => feedbacks,
+
+  __delay: ({ delay }) => delay
+}
+
+const mutations = {
+  SET_FEEDBACKS: (state, feedbacks) => (state.feedbacks = feedbacks),
+
+  SET_DELAY: (state, delay) => (state.delay = delay)
+}
+
+const actions = {
+  FEEDBACK_DELAY: ({ commit }, delay) => {
+    commit('SET_DELAY', delay)
   },
 
-  getters: {
-    __feedbacks: ({ feedbacks }) => feedbacks,
+  FEEDBACKS_ADD: ({ commit, getters, dispatch }, payload) => {
+    const isError = payload instanceof Error
 
-    __delay: ({ delay }) => delay
-  },
-
-  mutations: {
-    SET_FEEDBACKS: (state, feedbacks) => (state.feedbacks = feedbacks),
-
-    SET_DELAY: (state, delay) => (state.delay = delay)
-  },
-
-  actions: {
-    FEEDBACK_DELAY: ({ commit }, delay) => {
-      commit('SET_DELAY', delay)
-    },
-
-    FEEDBACKS_ADD: ({ commit, getters, dispatch }, payload) => {
-      const isError = payload instanceof Error
-
-      const feedback = {
-        type: payload.type || (isError && 'error'),
-        message: payload.message,
-        highlighted: payload.highlighted
-      }
-
-      commit('SET_FEEDBACKS', [ ...getters['__feedbacks'], feedback ])
-
-      setTimeout(() => { dispatch('FEEDBACK_REMOVE', feedback) }, getters['__delay'])
-    },
-
-    FEEDBACK_REMOVE: ({ commit, getters }, payload) => {
-      const feedbacks = getters['__feedbacks'].filter(feedback => feedback !== payload)
-
-      commit('SET_FEEDBACKS', feedbacks)
+    const feedback = {
+      type: payload.type || (isError && 'error'),
+      message: payload.message || 'error message',
+      highlighted: payload.highlighted || 'error highlighted message'
     }
+
+    commit('SET_FEEDBACKS', [ ...getters['__feedbacks'], feedback ])
+
+    setTimeout(() => { dispatch('FEEDBACK_REMOVE', feedback) }, getters['__delay'])
+  },
+
+  FEEDBACK_REMOVE: ({ commit, getters }, payload) => {
+    const feedbacks = getters['__feedbacks'].filter(feedback => feedback !== payload)
+
+    commit('SET_FEEDBACKS', feedbacks)
   }
 }
+
+export default { state, getters, mutations, actions }
